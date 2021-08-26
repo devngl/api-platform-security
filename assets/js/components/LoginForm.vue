4fcdf3baa8f1d61b3132015a060cb3f9b97cfd93
Ángel Marqués <g89.angel@gmail.com>
2021-08-26 18:03:06 +0200

    First login tests

--- assets/js/components/LoginForm.vue
+++ assets/js/components/LoginForm.vue
@@ -39,7 +39,6 @@
                 this.isLoading = true;
                 this.error = '';
 
-                /*
                 axios
                     .post('/login', {
                         email: this.email,
@@ -52,11 +51,14 @@
                         //this.email = '';
                         //this.password = '';
                     }).catch(error => {
-                        console.log(error.response.data);
+                        if (error.response.data.error) {
+                          this.error = error.response.data.error
+                        } else {
+                          this.error = 'Unknown error'
+                        }
                     }).finally(() => {
                         this.isLoading = false;
                     })
-                */
             },
         },
     }
